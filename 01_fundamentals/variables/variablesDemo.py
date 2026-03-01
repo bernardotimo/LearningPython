42510f9a81866b968cd27faf156a4ddf54169352
bernardo12345678 <bernardomota208@gmail.com>
2026-03-01 13:34:34 -0300

    fix: variables adjustment

--- 01_fundamentals/variables/variablesDemo.py
+++ 01_fundamentals/variables/variablesDemo.py
@@ -1,17 +1,6 @@
 #Stores and reuse information
 #Python assigns data types automatically
 
-#Strings
-
-phrase = "Hello, World"
-name = 'Bernardo'
-print(phrase)
-print(name)
-
-#Doesn't work with single quote when the phrase has an apostrophe
-#Variable with two names must be written with an underscore
-#bakery_name = 'Carl's bakery'#
-
 #Integer
 ingredient_quantity = 2
 print(ingredient_quantity)
